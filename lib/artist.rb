f5a2d7fe4f65c6b563e00792c04603b5e2808417
Ryan Mui <reachryanmui@gmail.com>
2019-03-02 15:51:22 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,15 @@
+class Artist
+  attr_accessor :name, :songs
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def add_song(name)
+    song = Song.new(name)
+    @songs << song
+  end
+
+
+
+end
